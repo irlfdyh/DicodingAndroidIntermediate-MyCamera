0787e41e3bb87f17721394a2a1290fb8527bfbe0
Irshal Firdeansyah <irshalfir@gmail.com>
2023-05-26 00:26:23 +0700

    implement upload file to server

--- app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
@@ -13,13 +13,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
-import androidx.core.net.toUri
 import com.dicoding.picodiploma.mycamera.databinding.ActivityMainBinding
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private var getFile: File? = null
 
     companion object {
         const val CAMERA_X_RESULT = 200
@@ -69,7 +77,40 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun uploadImage() {
-        Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+        if (getFile != null) {
+            val file = getFile as File
+
+            val description = "Ini adalah deskripsi gambar".toRequestBody("text/plain".toMediaType())
+            val requestImageFile = file.asRequestBody("image/jpeg".toMediaType())
+            val imageMultipart: MultipartBody.Part = MultipartBody.Part.createFormData(
+                "photo",
+                file.name,
+                requestImageFile
+            )
+
+            val apiService = ApiConfig().getApiService()
+            val uploadImageRequest = apiService.uploadImage(imageMultipart, description)
+            uploadImageRequest.enqueue(object : Callback<FileUploadResponse> {
+                override fun onResponse(
+                    call: Call<FileUploadResponse>,
+                    response: Response<FileUploadResponse>,
+                ) {
+                    if (response.isSuccessful) {
+                        val responseBody = response.body()
+                        if (responseBody != null && !responseBody.error) {
+                            Toast.makeText(this@MainActivity, responseBody.message, Toast.LENGTH_SHORT).show()
+                        }
+                    } else {
+                        Toast.makeText(this@MainActivity, response.message(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+                override fun onFailure(call: Call<FileUploadResponse>, t: Throwable) {
+                    Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_SHORT).show()
+                }
+            })
+        } else {
+            Toast.makeText(this@MainActivity, "Silakan masukkan berkas gambar terlebih dahulu.", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun startGallery() {
@@ -116,6 +157,7 @@ class MainActivity : AppCompatActivity() {
 
             myFile?.let { file ->
                 rotateFile(file, isBackCamera)
+                getFile = file
                 binding.previewImageView.setImageBitmap(BitmapFactory.decodeFile(file.path))
             }
         }
@@ -129,6 +171,7 @@ class MainActivity : AppCompatActivity() {
             val myFile = File(currentPhotoPath)
             myFile.let { file ->
                 rotateFile(file)
+                getFile = file
                 binding.previewImageView.setImageBitmap(BitmapFactory.decodeFile(file.path))
             }
         }
@@ -141,7 +184,8 @@ class MainActivity : AppCompatActivity() {
             val selectedImage = result?.data?.data as Uri
             selectedImage.let { uri ->
                 val myFile = uriToFile(uri, this@MainActivity)
-                binding.previewImageView.setImageURI(myFile.toUri())
+                getFile = myFile
+                binding.previewImageView.setImageURI(uri)
             }
         }
     }
